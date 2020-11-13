ed476e00a357ede6894d57f660d1939dbd543f7d
turnerjake <jaketurner@mail.fresnostate.edu>
2020-11-12 16:24:27 -0800

    Structured English Commit

---
+++ test_one.py
@@ -0,0 +1,12 @@
+"""
+STRUCTURED ENGLISH
+
+User has two salary options:
+10 days of work for $100/day
+OR
+10 days of work with pay doubling each day, starting from $1/day
+
+Use functions to calculate total salaries for both options. Compare outputs of both functions and & print the greater one to the user.
+
+END STRUCTURED ENGLISH
+"""
